5c8bceb762a78f259751a38dc3a30039f1fa96fa
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-09-06 15:28:34 +0200

    fix icons

--- src/app.js
+++ src/app.js
@@ -26,6 +26,10 @@ window.addToHomescreen({
 
 document.querySelector('.center-btn--icon').src = icons.Center;
 document.querySelector('.nav-white').src = icons.NavWhite;
+document.querySelector('#next-instruction-arrow-img').src = icons.Arrow;
+document.querySelector('#swoosh-left').src = icons.SwooshLeft;
+document.querySelector('#swoosh-right').src = icons.SwooshRight;
+document.querySelector('#bike').src = icons.Bike;
 
 router.initialize();
 
